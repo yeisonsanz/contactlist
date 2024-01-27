27855ac8a30eac644d05f1c486eea9dd4b9828a3
yeisonsanz <yeisonkb@gmail.com>
2024-01-27 04:15:08 -0500

    iniciando proyecto

---
+++ src/main/java/com/crud/contactlistapi/ContactlistapiApplication.java
@@ -0,0 +1,13 @@
+package com.crud.contactlistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactlistapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactlistapiApplication.class, args);
+    }
+
+}
